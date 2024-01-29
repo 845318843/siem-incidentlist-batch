1bf1227d597c8dcec33f25b26cebc52d8cded795
steven-hawking <845318843@QQ.COM>
2024-01-29 15:23:12 +0800

    version 1.0
    version 1.0 , batch post

---
+++ Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace batchHandle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_run = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_run
+            // 
+            this.btn_run.Font = new System.Drawing.Font("宋体", 11F);
+            this.btn_run.Location = new System.Drawing.Point(309, 164);
+            this.btn_run.Name = "btn_run";
+            this.btn_run.Size = new System.Drawing.Size(45, 48);
+            this.btn_run.TabIndex = 0;
+            this.btn_run.Text = "批量处置";
+            this.btn_run.UseVisualStyleBackColor = true;
+            this.btn_run.Click += new System.EventHandler(this.btn_run_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(332, 60);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJydG0iLCJpZCI6MSwibmFtZSI6ImFkbWluIiwiaWF0IjoxNzA2" +
+    "NDkyMzkwLCJleHAiOjE3MDcwOTcxOTB9.dcRIIErpSe6j3JEA9Zku_pNxRlivXJLCt3BmXi1vdfM";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("宋体", 11F);
+            this.textBox2.Location = new System.Drawing.Point(13, 78);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(290, 240);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "b4cf472d1f25e06c0d327a17ffb55ae8\r\nes_index_id\":\"([^\"]*)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(361, 323);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_run);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "中国通建态势日志批处理";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_run;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace batchHandle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void btn_run_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string[] advices = { 
+                "对该IP进行溯源后，发现并未受到影响。",
+                "跟踪发现，该攻击并未持续，未受到影响",
+                "在边界设备上将其加入黑名单封30分钟" };
+                string[] str = new string[textBox2.Lines.Length];
+                for (int i = 0; i < textBox2.Lines.Length; i++)
+                {
+                    str[i] = textBox2.Lines[i].Trim();
+                    if (!httpPost.Post(str[i], advices[i % 3], textBox1.Text).Contains("成功"))
+                    {
+                        MessageBox.Show("批量处理失败！" + str[i].ToString());
+                    }
+                }
+                MessageBox.Show("批量处理全部完成！");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("程序出现错误");
+            }
+          
+        }
+    }
+}
